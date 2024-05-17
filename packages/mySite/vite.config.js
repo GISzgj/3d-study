52e1b70d591efcb3bf4ed422caace7cc79551ab5
zgj <1363752163@qq.com>
2024-05-17 17:54:08 +0800

    init

--- packages/mySite/vite.config.js
+++ packages/mySite/vite.config.js
@@ -2,14 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import { resolve } from 'path'
+const r = (...args) => resolve(__dirname, '.', ...args)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: r('src/auto-imports.d.ts')
+    })
   ],
   resolve: {
     alias: {
+      '~': `${resolve(__dirname, './')}`,
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
